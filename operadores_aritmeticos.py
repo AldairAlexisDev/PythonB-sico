748e7f57382a069072d38906e5252bde09bc32bc
Aldair Alexis <aalexisdev11@gmail.com>
2024-09-17 09:57:36 -0500

    Viendo Operadores Aritméticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,33 @@
+#Variables
+a = 15
+x = 2
+y = 10
+z = 7
+
+#Adición
+suma = x + y
+print(suma)
+
+#Resta
+resta = y - x
+print(resta)
+
+#Multiplicación
+multiplicacion = x * y
+print(multiplicacion)
+
+#Division
+division = z / x
+print(division)
+
+#Modulo / residuo de la división
+modulo = y % z
+print(modulo)
+
+#Exponentes
+exponente = x ** y
+print(exponente)
+
+#Redondeo al número más cercano
+redondeo = a // 15
+print(redondeo)
