45dc5aac051050189767e835a7ed11ffaff53270
Aldair Alexis <aalexisdev11@gmail.com>
2024-09-17 12:31:58 -0500

    Condicionales if_else

---
+++ condicionales_ifelse.py
@@ -0,0 +1,9 @@
+
+#Verificar si una persona es mayor de edad:
+
+edad = int(input("Ingrese su edad: "))
+
+if(edad >= 18):
+    print("Usted es mayor de edad.")
+else:
+    print("Usted aún es menor de edad.")
