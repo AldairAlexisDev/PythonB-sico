87b1053a8a029b69d734fabfbc25c07998d4c0de
Aldair Alexis <aalexisdev11@gmail.com>
2024-09-17 09:46:44 -0500

    Viendo variable en Python

---
+++ variables.py
@@ -0,0 +1,17 @@
+#Las variables son utilizadas como contenedores para guardar valores de datos.
+#En python una variable se crea inmediataente cuando se le asigna un valor
+
+#Ejemplo:
+x = 5
+print(x) #Utilizamos la función print() para imprimir la variable "x" y se obtiene su valor 5
+print(type(x)) #Utilizamos la función type para ver el tipo de dato que es la variable x y en
+               #en este caso es un int 
+#Mas variables:
+nombre = "Alex" #Tipo String / str
+promedio_Notas = 18.5 #Tipo float
+
+#Tipo boolean (Solo tiene dos valores True o False)
+4 == 4  #Compara y si es verdadero devuelve True, sino False
+
+print(type(nombre))
+print(type(promedio_Notas))
