4a48c539ecfc832713b66a398d853eb82b573ee6
Aldair Alexis <aalexisdev11@gmail.com>
2024-09-19 18:18:34 -0500

    Se agregó mas ejemplos

--- diccionarios.py
+++ diccionarios.py
@@ -9,3 +9,11 @@ print(dic)
 
 #Busqueda de valores
 print(dic["Blue"])
+
+#Modificar valores
+dic["Red"] = "White"
+dic.update({"Blue":"Azul Claro"})
+
+#Eliminar un elemento
+del dic["Blue"]
+dic.pop("Yellow")
