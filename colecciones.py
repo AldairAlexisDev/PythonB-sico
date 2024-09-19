2eb690bea628fcba2c9509a48e64eff8db30d26f
Aldair Alexis <aalexisdev11@gmail.com>
2024-09-19 14:02:31 -0500

    Algunas colecciones

---
+++ colecciones.py
@@ -0,0 +1,11 @@
+# COLECCIONES +
+
+#DICCIONARIOS
+ciudades = {"Lima","Quito","Santiago","Buenos Aires"}
+
+#TUPLAS
+costumbres = ("Cocinar","Jugar","Correr")
+
+#SETS
+habitantes = {"20500","30000","8000"}
+
